8b44b9c28d17b4888c38946bad2bd7684ff04027
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-05-12 17:14:45 +0900

    #5 Controller testの実装

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,51 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
+
+  test "ユーザー一覧の表示" do
+    get users_url
     assert_response :success
+  end
+
+  test "ユーザーを新規作成するページの表示" do
+    get new_user_url
+    assert_response :success
+  end
+
+  test "ユーザーを作成時にレコードが一件増えること" do
+    assert_difference("User.count") do
+      post users_url, params: { user: { name: "test_user" } }
+    end
+    assert_response :redirect
+  end
+
+
+  test "ユーザーの詳細を表示できるか" do
+    @user = users(:test_user)
+    get user_url(@user)
+    assert_response :success
+  end
+
+  test "ユーザー編集ページを表示できるか" do
+    @user = users(:test_user)
+    get edit_user_url(@user)
+    assert_response :success
+  end
+
+
+  test "ユーザー情報を更新できるか" do
+    @user = users(:test_user)
+    patch user_url(@user), params: { user: { name: "updated_user" } }
+    assert_redirected_to user_url(@user)
+    @user.reload
+    assert_equal "updated_user", @user.name
+  end
+
+  test "ユーザーを削除できるか" do
+    @user = users(:test_user)
+    assert_difference("User.count", -1) do
+      delete user_url(@user)
+    end
+    assert_redirected_to users_url
   end
 end
