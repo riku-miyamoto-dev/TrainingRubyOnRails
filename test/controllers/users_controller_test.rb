e9c7429041c19655cb609030d0a3891f80eea70d
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-05-14 10:07:06 +0900

    #5 不要なインスタンス変数を変数に変更

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,34 +16,33 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_difference("User.count") do
       post users_url, params: { user: { name: "test_user" } }
     end
-    assert_response :redirect
+    assert_redirected_to user_url(User.last)
   end
 
-
   test "ユーザーの詳細を表示できるか" do
-    @user = users(:test_user)
-    get user_url(@user)
+    user = users(:test_user)
+    get user_url(user)
     assert_response :success
   end
 
   test "ユーザー編集ページを表示できるか" do
-    @user = users(:test_user)
-    get edit_user_url(@user)
+    user = users(:test_user)
+    get edit_user_url(user)
     assert_response :success
   end
 
   test "ユーザー情報を更新できるか" do
-    @user = users(:test_user)
-    put user_url(@user), params: { user: { name: "updated_user" } }
-    assert_redirected_to user_url(@user)
-    @user.reload
-    assert_equal "updated_user", @user.name
+    user = users(:test_user)
+    patch user_url(user), params: { user: { name: "updated_user" } }
+    assert_redirected_to user_url(user)
+    user.reload
+    assert_equal "updated_user", user.name
   end
 
   test "ユーザーを削除できるか" do
-    @user = users(:test_user)
+    user = users(:test_user)
     assert_difference("User.count", -1) do
-      delete user_url(@user)
+      delete user_url(user)
     end
     assert_redirected_to users_url
   end
