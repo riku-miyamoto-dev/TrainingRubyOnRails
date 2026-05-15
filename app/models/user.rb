14edb75c97b8f893bc00d1d03aecf1e149053f3d
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-05-15 16:59:46 +0900

    #7 ユーザー情報を管理するためのCRUDの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
-end
+  validates :name, :furigana, :gender, :tell, :email, :post_number, :prefecture, :city, :town, :street_address, :birthday, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :name, length: { maximum: 50 }
+  end
---
+++ db/migrate/20260515051251_add_column_to_user.rb
@@ -0,0 +1,16 @@
+class AddColumnToUser < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :furigana, :string
+    add_column :users, :gender, :string
+    add_column :users, :tell, :string
+    add_column :users, :phone, :string
+    add_column :users, :email, :string
+    add_column :users, :post_number, :string
+    add_column :users, :prefecture, :string
+    add_column :users, :city, :string
+    add_column :users, :town, :string
+    add_column :users, :street_address, :string
+    add_column :users, :building, :string
+    add_column :users, :birthday, :date
+  end
+end
