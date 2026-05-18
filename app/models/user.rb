3c6a8c23be5367ec6af6d2722101d87f4f9c318d
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-05-18 11:36:30 +0900

    #7 エラーの表示実装(仮),およびvalidationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
   validates :name, :furigana, :gender, :tell, :email, :post_number, :prefecture, :city, :town, :street_address, :birthday, presence: true
   validates :name, length: { minimum: 2 }
   validates :name, length: { maximum: 50 }
+
+  validates :email, :tell, uniqueness: true
+
+  validates_each :birthday do |record, attr, value|
+    record.errors.add(attr, "では未来の日付を設定できません")if value > Date.today
+  end
+
+  # 電話番号の最長が11桁で間にハイフンが入っているため、最長13桁
+  validates :phone, :tell,length:{ maximum: 13 }
+
+    
+  enum :gender, male: "Male", female: "Female", other: "Others"
   end
