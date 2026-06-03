52d6192bd4b6ceb9f9817f0d2968666e84fd364d
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-03 10:16:50 +0900

    #15 validation不備を修正

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,5 @@ class Skill < ApplicationRecord
   has_many :users_skills, dependent: :destroy
   has_many :users, through: :users_skills
   validates :name, presence: { message: "名前は必須です" }
+  validates :name, uniqueness:{ message: "すでに登録されているスキル名です"}
 end
