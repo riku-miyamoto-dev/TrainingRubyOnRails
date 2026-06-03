e5e90127e9f7acb908c6860c3c23a27dbcc686a0
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-03 15:33:43 +0900

    #15 adminのcontrollerテスト実装

---
+++ test/controllers/admin/skills_controller_test.rb
@@ -0,0 +1,59 @@
+require "test_helper"
+
+class Admin::SkillsControllerTest < ActionDispatch::IntegrationTest
+  test "スキル一覧の表示" do
+    get admin_skills_url
+    assert_response :success
+  end
+
+  test "新規作成ページの表示" do
+    get new_admin_skill_url
+    assert_response :success
+  end 
+
+  test "スキル作成時にレコードが一件増えること" do
+    assert_difference("Skill.count") do
+      post admin_skills_url, params: { 
+        skill: { 
+          name: "世界遺産検定",
+        } 
+      }
+    end
+    assert_redirected_to admin_skill_url(Skill.last)
+  end  
+
+
+
+  test "スキル編集ページを表示できるか" do
+    @skill = skills(:language)
+    get edit_admin_skill_url(@skill)
+    assert_response :success
+  end
+
+  test "スキルの詳細を表示できるか" do
+    @skill = skills(:language)
+    get skill_url(@skill)
+    assert_response :success
+  end
+
+  test "情報を更新できるか" do
+    @skill = skills(:language)
+    patch admin_skill_url(@skill), params: { 
+      skill: { 
+        name: "Ruby",
+      } 
+    }
+    assert_redirected_to admin_skill_url(@skill)
+    @skill.reload
+    assert_equal "Ruby", @skill.name
+  end
+  
+  test "スキルを削除できるか" do
+    skill = skills(:language)
+    assert_difference("Skill.count", -1) do
+      delete admin_skill_url(skill)
+    end
+    assert_redirected_to admin_skills_url
+  end
+
+end
