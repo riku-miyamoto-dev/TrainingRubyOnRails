a5247d8965e4b7aa53dd1f4c381af4b4b2e42fc8
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-03 10:16:22 +0900

    #15 admin用にcontrollerを作成

---
+++ app/controllers/admin/departments_controller.rb
@@ -0,0 +1,52 @@
+class Admin::DepartmentsController < ApplicationController
+def index
+    @department = Department.all
+  end
+
+  def show
+    @department = Department.find(params[:id])
+  end
+
+  def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(department_params)
+    if @department.save
+      redirect_to [:admin, @department]
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @department = Department.find(params[:id])
+  end
+
+  def update
+    @department = Department.find(params[:id])
+    if @department.update(department_params)
+      redirect_to [:admin, @department]
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @department = Department.find(params[:id])
+    if @department.destroy
+      redirect_to admin_departments_path
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  
+  private
+    def department_params
+      params.expect(department: [ 
+        :name 
+      ])
+    end
+end
---
+++ app/controllers/admin/skills_controller.rb
@@ -0,0 +1,50 @@
+class Admin::SkillsController < ApplicationController
+
+  def index
+    @skills = Skill.all
+  end
+
+  def show
+    @skill = Skill.find(params[:id])
+  end
+
+  def new
+    @skill = Skill.new
+  end
+
+  def create
+    @skill = Skill.new(skill_params)
+    if @skill.save
+      redirect_to [:admin, @skill]
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @skill = Skill.find(params[:id])
+  end
+
+  def update
+    @skill = Skill.find(params[:id])
+    if @skill.update(skill_params)
+      redirect_to [:admin, @skill]
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @skill = Skill.find(params[:id])
+    @skill.destroy
+    redirect_to admin_skills_path
+  end
+
+  private
+
+  def skill_params
+    params.expect(skill: [
+      :name 
+    ])
+  end
+end
