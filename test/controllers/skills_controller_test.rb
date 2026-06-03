39734be161d048c6320287f8f20bdc8de1c11f22
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-03 15:33:56 +0900

    #15 通常のcontrollerテスト実装

--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -6,52 +6,9 @@ class SkillsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "新規作成ページの表示" do
-    get new_skill_url
-    assert_response :success
-  end 
-
-  test "スキル作成時にレコードが一件増えること" do
-    assert_difference("Skill.count") do
-      post skills_url, params: { 
-        skill: { 
-          name: "PD1",
-        } 
-      }
-    end
-    assert_redirected_to skill_url(Skill.last)
-  end  
-
-  test "スキル編集ページを表示できるか" do
-    @skill = skills(:language)
-    get edit_skill_url(@skill)
-    assert_response :success
-  end
-
   test "スキルの詳細を表示できるか" do
     @skill = skills(:language)
     get skill_url(@skill)
     assert_response :success
   end
-
-  test "情報を更新できるか" do
-    @skill = skills(:language)
-    patch skill_url(@skill), params: { 
-      skill: { 
-        name: "Ruby",
-      } 
-    }
-    assert_redirected_to skill_url(@skill)
-    @skill.reload
-    assert_equal "Ruby", @skill.name
-  end
-  
-  test "スキルを削除できるか" do
-    skill = skills(:language)
-    assert_difference("Skill.count", -1) do
-      delete skill_url(skill)
-    end
-    assert_redirected_to skills_url
-  end
-
 end
