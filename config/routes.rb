f2b2dbcca2584612b90388aa922786671f46ae20
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-05 15:33:22 +0900

    #17 画像のURL用のルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   resources :departments, only: [:index, :show]
   resources :skills, only: [:index, :show]
   resources :users, only: [:index, :show]
+  get "users/:id/image", to: "users#show_image", as: "user_image"
 
   namespace :admin do
     root "users#index"
     resources :departments
     resources :skills
     resources :users
+    get "users/:id/image", to: "users#show_image", as: "user_image"
   end
 
 end
