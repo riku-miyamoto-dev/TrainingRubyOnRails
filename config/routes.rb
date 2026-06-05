f914c301ca3fad9ccd62fdc148931e51083dac8f
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-05 17:20:39 +0900

    #17memberを用いて可読性を向上

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,23 @@ Rails.application.routes.draw do
   root "users#index"
   resources :departments, only: [:index, :show]
   resources :skills, only: [:index, :show]
-  resources :users, only: [:index, :show]
-  get "users/:id/image", to: "users#show_image", as: "user_image"
+  resources :users, only: [:index, :show]do
+      member do
+        get "show_image"
+      end
+    end
+  # get "users/:id/image", to: "users#show_image", as: "user_image"
 
   namespace :admin do
     root "users#index"
     resources :departments
     resources :skills
-    resources :users
-    get "users/:id/image", to: "users#show_image", as: "user_image"
+    resources :users do
+      member do
+        get "show_image"
+      end
+    end
+    # get "users/:id/image", to: "users#show_image", as: "user_image"
   end
 
 end
