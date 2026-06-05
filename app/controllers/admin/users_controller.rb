be9c56825c1d4691c6b140c6cf9b0609002f411a
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-05 11:57:29 +0900

    #17 rails ユーザーの入力値を変更しない実装に変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,11 +13,14 @@ class Admin::UsersController < Admin::ApplicationController
 
   def create
     @user = User.new(user_params)
-    image_file = user_params[:user_img]
-    
-    if image_file.present?
-      @user.user_img = image_file.read
-      @user.user_img_extension = image_file.content_type  
+    @user.assign_attributes(user_params)
+
+    if params[:user][:image].present?
+      @user.image = params[:user][:image].read
+    end
+
+    if params[:user][:image].present?
+      @user.image_extension = params[:user][:image].content_type 
     end
 
     if @user.save
@@ -34,16 +37,17 @@ class Admin::UsersController < Admin::ApplicationController
 
   def update
     @user = User.find(params[:id])
-    image_record = user_params
+    @user.assign_attributes(user_params)
     
-    if params[:user][:user_img].present?
-      update_img = params[:user][:user_img]
+    if params[:user][:image].present?
+      @user.image = params[:user][:image].read
+    end
 
-      image_record[:user_img] = update_img.read
-      image_record[:user_img_extension] = update_img.content_type 
+    if params[:user][:image].present?
+      @user.image_extension = params[:user][:image].content_type 
     end
     
-    if @user.update(image_record)
+    if @user.save
       redirect_to [:admin, @user]
     else
       render :edit, status: :unprocessable_entity
@@ -74,8 +78,6 @@ class Admin::UsersController < Admin::ApplicationController
         :birthday,
         :department_id, 
         :user_skill_id, 
-        :user_img,
-        :user_img_extension,
         skill_ids:[],
       ])
     end
