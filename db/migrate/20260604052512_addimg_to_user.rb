9cf362aa2a0fb5e0cb2877831a4c4fe0a92a996e
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-08 10:18:18 +0900

    #17 容量制限をdbにて追加

--- db/migrate/20260604052512_addimg_to_user.rb
+++ db/migrate/20260604052512_addimg_to_user.rb
@@ -1,6 +1,6 @@
 class AddimgToUser < ActiveRecord::Migration[8.1]
   def change
-    add_column :users, :image, :binary
+    add_column :users, :image, :binary, limit: 50.megabytes
     add_column :users, :image_extension, :string
   end
 end
