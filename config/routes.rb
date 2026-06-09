4440b75c60eadfbd5fd4ef01f5d8a5c7405b9ea8
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-09 10:35:55 +0900

    #20  ネストしたルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,32 @@
 Rails.application.routes.draw do
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+
   root "users#index"
-  resources :departments, only: [:index, :show]
-  resources :skills, only: [:index, :show]
-  resources :users, only: [:index, :show]do
+  resources :users, only: [:index, :new, :create, :show], concerns: :paginatable
+  resources :departments, only: [:index, :show] do
+    resources :users, only: [:index, :show], concerns: :paginatable do
       member do
         get "image"
       end
     end
+  end
+  resources :skills, only: [:index, :show]
+  resources :users, only: [:index, :new, :create]
   # get "users/:id/image", to: "users#show_image", as: "user_image"
 
   namespace :admin do
     root "users#index"
-    resources :departments
+    resources :users, concerns: :paginatable
     resources :skills
-    resources :users do
-      member do
-        get "image"
+    resources :departments do
+      resources :users, concerns: :paginatable do
+        member do
+          get "image"
+        end
       end
     end
     # get "users/:id/image", to: "users#show_image", as: "user_image"
   end
-
 end
