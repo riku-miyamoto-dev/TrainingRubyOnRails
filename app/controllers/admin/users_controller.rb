ef80bcd384cf69b6b478a267b2fc3a650a3097d0
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-09 11:59:31 +0900

    #20  マジックナンバーの削除と configの設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
   def index
-    @users = User.order(:name).page(params[:page]).per(100)
+    @users = User.order(:name).page(params[:page])
   end
 
   def show
