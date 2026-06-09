bf853d0fa9afcfb5766edc0f8d70cedef5eae75a
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-09 15:31:13 +0900

    #22 name,prefecture検索フォームの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,18 @@ class UsersController < ApplicationController
   
   def index
     @users = User.order(:name).page(params[:page]).per(params[:per_page])
+    if params[:search].present?  
+      if @users.present?
+        if params[:search][:query_name].present?
+          @users = @users.where("name LIKE ?",
+          User.sanitize_sql_like(params[:search][:query_name]) + "%")
+        end
+        if params[:search][:query_prefecture].present?
+          @users = @users.where(prefecture: params[:search][:query_prefecture])
+        end
+      end
+    end
   end
-
   def show
     @user = User.find(params[:id])
   end
