f7f8b9b518218a015f3dc3b24813edf450ffa166
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-10 14:45:10 +0900

    #22 表示件数と絞り込みや並び替えのsubmit分離

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    if params[:search][:query_name].present?
+    if params.dig(:search, :query_name).present?
       @users = @users.where("name LIKE ?",
       User.sanitize_sql_like(params[:search][:query_name]) + "%")
     end
 
-    if params[:search][:query_prefecture].present?
+    if params.dig(:search, :query_prefecture).present?
       @users = @users.where(prefecture: params[:search][:query_prefecture])
     end
 
-    if params[:search][:sort_birthday].present? && params[:search][:sort_birthday] == "生年月日 昇順"
+    if params.dig(:search, :sort_birthday).present?.present? && params.dig(:search, :sort_birthday) == "生年月日 昇順"
       @users = @users.order(birthday: :asc)
-    elsif params[:search][:sort_birthday].present? && params[:search][:sort_birthday] == "生年月日 降順"
+    elsif params.dig(:search, :sort_birthday).present? && params.dig(:search, :sort_birthday) == "生年月日 降順"
       @users = @users.order(birthday: :desc)
     end
 
     if (
-      params[:search][:between_birthday_fast].present?
-      && params[:search][:between_birthday_late].present?
-      &&  params[:search][:between_birthday_fast] > params[:search][:between_birthday_late]
+      params.dig(:search, :between_birthday_fast).present?
+      && params.dig(:search, :between_birthday_late).present?
+      &&  params.dig(:search, :between_birthday_fast) > params.dig(:search, :between_birthday_late)
       )
-      @users = @users.where(birthday: params[:search][:between_birthday_late]..params[:search][:between_birthday_fast])
+      @users = @users.where(birthday: params.dig(:search, :between_birthday_late)..params.dig(:search, :between_birthday_fast))
     end
-    @users = @users.order(:name).page(params[:page]).per(params[:per_page])
+    @users = @users.order(:name).page(params.dig(:page)).per(params.dig(:per_page))
   end
   
   def show
