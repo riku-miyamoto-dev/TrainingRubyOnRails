3ee4efb38eea19adb9d69cb5d47573b3171d6e85
riku-miyamoto-dev <riku.miyamoto@rizapgroup.com>
2026-06-12 11:00:29 +0900

    #22　scopeの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,33 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    if params.dig(:search, :query_name).present?
+    if params[:name].present?
       @users = @users.where("name LIKE ?",
-      User.sanitize_sql_like(params[:search][:query_name]) + "%")
+      User.sanitize_sql_like(params[:name]) + "%")
     end
 
-    if params.dig(:search, :query_prefecture).present?
-      @users = @users.where(prefecture: params[:search][:query_prefecture])
+    if params[:prefecture].present?
+      @users = @users.where(prefecture: params[:prefecture])
     end
 
-    if params.dig(:search, :sort).present? && params.dig(:search, :sort) == "生年月日 昇順"
+    if params[:sort].present? && params[:sort] == "生年月日 昇順"
       @users = @users.order(birthday: :asc)
-    elsif params.dig(:search, :sort).present? && params.dig(:search, :sort) == "生年月日 降順"
+    elsif params[:sort].present? && params[:sort] == "生年月日 降順"
       @users = @users.order(birthday: :desc)
-    elsif params.dig(:search, :sort).present? && params.dig(:search, :sort) == "id 昇順"
+    elsif params[:sort].present? && params[:sort] == "id 昇順"
       @users = @users.id_sort_ascending_order
-    elsif params.dig(:search, :sort).present? && params.dig(:search, :sort) == "id 降順"
+    elsif params[:sort].present? && params[:sort] == "id 降順"
       @users = @users.id_sort_descending_order
     end
 
     if (
-      params.dig(:search, :between_birthday_fast).present?
-      && params.dig(:search, :between_birthday_late).present?
-      &&  params.dig(:search, :between_birthday_fast) > params.dig(:search, :between_birthday_late)
-      )
-      @users = @users.where(birthday: params.dig(:search, :between_birthday_late)..params.dig(:search, :between_birthday_fast))
+      params[:between_birthday_fast].present?
+      && params[:between_birthday_late].present?
+      &&  params[:between_birthday_fast] > params[:between_birthday_late]
+    )
+      @users = @users.where(birthday: params[:between_birthday_late]..params[:between_birthday_fast])
     end
-    @users = @users.order(:name).page(params.dig(:page)).per(params.dig(:per_page))
+    @users = @users.order(:name).page(params[:page]).per(params[:per_page])
   end
   
   def show
